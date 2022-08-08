3e67aa1e93b8b0bcf8d81d05e47375b26601efed
mirsahib <mirsahib24@gmail.com>
2022-08-08 18:31:18 +0600

    :tada: initial commit

---
+++ src/MapboxAutocomplete.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MapboxAutocomplete = () => {
+  return (
+    <View>
+      <Text>MapboxAutocomplete</Text>
+    </View>
+  )
+}
+
+export default MapboxAutocomplete
+
+const styles = StyleSheet.create({})
